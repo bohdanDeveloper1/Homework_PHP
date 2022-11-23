fa915fbd870943b48b8dc4e3ac950a69c29a1c24
Jakub Bilski <jakub@blsk.pl>
2022-11-23 11:31:23 +0100

    Snapshot of work on Response class.

--- src/index.php
+++ src/index.php
@@ -3,6 +3,10 @@
 require_once 'vendor/autoload.php';
 
 use Apsl\Http\Request;
+use Apsl\Http\Response;
 
 
 $request = new Request();
+
+$response = new Response();
+$response->addHeader(Response::HEADER_CONTENT_TYPE, 'application/json');
--- src/lib/Apsl/Http/Response.php
+++ src/lib/Apsl/Http/Response.php
@@ -5,5 +5,34 @@ namespace Apsl\Http;
 
 class Response
 {
+    const HEADER_CONTENT_TYPE = 'Content-Type';
+    const HEADER_CONTENT_LENGTH = 'Content-Length';
 
+    protected array $headers = [];
+    protected string $body = '';
+
+    public function getHeaders(): array
+    {
+        return $this->headers;
+    }
+
+    public function addHeader(string $name, string $value): void
+    {
+        $this->headers[$name] = $value;
+    }
+
+    public function deleteHeader(string $name): void
+    {
+        unset($this->headers[$name]);
+    }
+
+    public function getBody(): string
+    {
+        return $this->body;
+    }
+
+    public function setBody(string $body): void
+    {
+        $this->body = $body;
+    }
 }
